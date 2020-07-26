1dd50915ee6266ed704c4d16d596274d32bdb91c
Brennan Predmore <brennnann@gmail.com>
2020-07-25 20:38:27 -0500

    google oAuth2 init // testing init

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
-const moongose = require('mongoose');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-moongose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 require('./routes/authRoutes')(app);
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Server listening on: http://localhost:${port}`);
-});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
